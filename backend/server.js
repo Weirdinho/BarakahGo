49610b4ec78e9c7029880a5e1b09cb601fec4a86
Weirdinho <ruruwhygee@gmail.com>
2026-06-21 16:30:43 +0100

    modified:   backend/node_modules/.package-lock.json 	modified:   backend/package-lock.json 	modified:   backend/package.json 	modified:   backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
+const path = require('path');
 
 dotenv.config(); 
 
